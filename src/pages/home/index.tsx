7e67f0ce3d020e1a0c01ac69ae6f26d3b1ac7deb
eileen819 <eileen.ju.8819@gmail.com>
2025-02-10 03:50:02 +0900

    컴포넌트 만들기 2

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,30 @@
 import Carousel from "components/Carousel";
 import PostList from "components/PostList";
+import { useState } from "react";
+
+type TabType = "all" | "my";
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState<TabType>("all");
   return (
     <>
       <Carousel />
       <div className="inner">
         <div className="post__navigation">
-          <div className="post__navigation--active">전체</div>
-          <div>나의 글</div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("all")}
+            className={activeTab === "all" ? "post__navigation--active" : ""}
+          >
+            전체
+          </div>
+          <div
+            role="presentation"
+            onClick={() => setActiveTab("my")}
+            className={activeTab === "my" ? "post__navigation--active" : ""}
+          >
+            나의 글
+          </div>
         </div>
         <PostList />
       </div>
